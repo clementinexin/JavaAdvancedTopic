aa65b7d596ef373a7498e5f87ad2da6ecf4f08d2
daijia <daijia1009@gmail.com>
2016-01-31 19:47:00 +0800

    rename package

---
+++ src/main/java/org/java/learn/topic/designpattern/observableinjdk/ConcreteObserver.java
@@ -0,0 +1,14 @@
+package org.java.learn.topic.designpattern.observableinjdk;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by DaiJia on 2015/12/27.
+ */
+public class ConcreteObserver implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("Being notified " + o);
+    }
+}
