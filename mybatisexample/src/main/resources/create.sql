2f44203c9ea6aea163330f9a0556c4ccc554a9a4
DaiJia <daijia1009@gmail.com>
2016-03-04 16:06:30 +0800

    mybatis reverse generator

---
+++ mybatisexample/src/main/resources/create.sql
@@ -0,0 +1,5 @@
+create database mybatis;
+use mybatis;
+CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
+INSERT INTO users(NAME, age) VALUES('孤傲苍狼', 27);
+INSERT INTO users(NAME, age) VALUES('白虎神皇', 27);
