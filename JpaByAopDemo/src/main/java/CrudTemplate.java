ef482a2c3e6b83a60889e5ced32cc3e16cde7666
DaiJia <daijia1009@gmail.com>
2016-03-22 17:37:38 +0800

    aop demo

---
+++ JpaByAopDemo/src/main/java/CrudTemplate.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: CrudTemplate <br/>
+ * Description: TODO <br/>
+ * Date: 2016-03-22 17:36 <br/>
+ * <br/>
+ *
+ * @author jdai@
+ *         <p/>
+ *         修改记录
+ * @version v1.0.1 2016-03-22 jdai@ created. <br/>
+ */
+public interface CrudTemplate {
+}
