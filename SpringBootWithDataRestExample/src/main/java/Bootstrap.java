8c4c43b7a7eb0a9a4d6f34c279f9ce87229c75c1
DaiJia <daijia1009@gmail.com>
2016-03-25 17:48:08 +0800

    创建一个SpringBoot应用

---
+++ SpringBootWithDataRestExample/src/main/java/Bootstrap.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: Bootstrap <br/>
+ * Description: TODO <br/>
+ * Date: 2016-03-25 17:44 <br/>
+ * <br/>
+ *
+ * @author jdai@
+ *         <p/>
+ *         修改记录
+ * @version v1.0.1 2016-03-25 jdai@ created. <br/>
+ */
+@SpringBootApplication
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class,args);
+    }
+}
