bfe466d98db24e111fdebc478ebc29d6d37665c1
daijiajia <daijiajia@meituan.com>
2016-05-13 15:21:30 +0800

    redis / imagemagick

---
+++ redis-demo/src/main/java/demo/SetnxDemo.java
@@ -0,0 +1,14 @@
+package demo;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by daijiajia on 16/5/12.
+ */
+public class SetnxDemo {
+
+    public static void main(String[] args) {
+        Jedis client = new Jedis("localhost");
+
+    }
+}
