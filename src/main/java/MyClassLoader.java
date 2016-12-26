d073f3d75b17be6bf7d5c29f7d3ce06150f3f282
daijiajia <daijiajia@meituan.com>
2016-12-26 14:12:19 +0800

    Files.readAllBytes(Paths.get(path + name.replace(".", "\\") + fileType))

--- src/main/java/MyClassLoader.java
+++ src/main/java/MyClassLoader.java
@@ -1,104 +1,104 @@
-
-
-import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import org.apache.commons.io.FileUtils;
 
 public class MyClassLoader extends ClassLoader {
 
-	private String name;
-	
-	private String path;
-	
-	public String getPath() {
-		return path;
-	}
+    private String name;
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    private String path;
 
-	private String fileType = ".class";
-	
-	public MyClassLoader(String name) {
-		super();
-		this.name = name;
-	}
-	
-	public MyClassLoader(ClassLoader parent,String name) {
-		super(parent);
-		this.name = name;
-	}
-	
-	@Override
-	protected Class<?> findClass(String name) throws ClassNotFoundException {
-		byte[] data = this.loadClassData(name);
-		
-		return this.defineClass(name, data, 0, data.length);
-	}
+    public String getPath() {
+        return path;
+    }
 
-	private byte[] loadClassData(String name) {
-		try {
-			return FileUtils.readFileToByteArray(new File(path + name.replace(".", "\\") + fileType));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public void setPath(String path) {
+        this.path = path;
+    }
 
-	@Override
-	public String toString() {
-		return "MyClassLoader [name=" + name + "]";
-	}
+    private String fileType = ".class";
 
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException {
-		MyClassLoader loader1 = new MyClassLoader("loader1");
-		loader1.setPath("D:\\classloader\\exercise\\lib1\\");
-		
-		MyClassLoader loader2 = new MyClassLoader(loader1,"loader2");
-		loader2.setPath("D:\\classloader\\exercise\\lib2\\");
-		
-		MyClassLoader loader3 = new MyClassLoader(null, "loader3");
-		loader3.setPath("D:\\classloader\\exercise\\otherlib\\");
-		
-		Class clazz = loader1.loadClass("Sample1");
-		
-		Object object = clazz.newInstance();
-		System.out.println(object.hashCode());
-		
-		object = null;
-		clazz = null;
-		loader1 = null;
-		
-		loader1 = new MyClassLoader("loader1");
-		loader1.setPath("D:\\classloader\\exercise\\lib1\\");
-		clazz = loader1.loadClass("Sample1");
-		object = clazz.newInstance();
-		System.out.println(object.hashCode());
-		
+    public MyClassLoader(String name) {
+        super();
+        this.name = name;
+    }
+
+    public MyClassLoader(ClassLoader parent, String name) {
+        super(parent);
+        this.name = name;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] data = this.loadClassData(name);
+
+        return this.defineClass(name, data, 0, data.length);
+    }
+
+    private byte[] loadClassData(String name) {
+        try {
+            return Files.readAllBytes(Paths.get(path + name.replace(".", "\\") + fileType));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClassLoader [name=" + name + "]";
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException {
+        MyClassLoader loader1 = new MyClassLoader("loader1");
+        System.out.println(loader1.getClass().getClassLoader());
+        loader1.setPath("D:\\classloader\\exercise\\lib1\\");
+
+        MyClassLoader loader2 = new MyClassLoader(loader1, "loader2");
+        System.out.println(loader2.getClass().getClassLoader());
+        loader2.setPath("D:\\classloader\\exercise\\lib2\\");
+
+        MyClassLoader loader3 = new MyClassLoader(null, "loader3");
+        System.out.println(loader3.getClass().getClassLoader());
+        loader3.setPath("D:\\classloader\\exercise\\otherlib\\");
+
+        Class clazz = loader1.loadClass("Sample1");
+
+        Object object = clazz.newInstance();
+        System.out.println(object.hashCode());
+
+        object = null;
+        clazz = null;
+        loader1 = null;
+
+        loader1 = new MyClassLoader("loader1");
+        loader1.setPath("D:\\classloader\\exercise\\lib1\\");
+        clazz = loader1.loadClass("Sample1");
+        object = clazz.newInstance();
+        System.out.println(object.hashCode());
+
 //		Sample1 sample1 = (Sample1) object;
 //		System.out.println(sample1.v1);
-		// 反射可以跨越loader域隔离的限制
-		
+        // 反射可以跨越loader域隔离的限制
+
 //		Field field = object.getClass().getField("v1");
 //		System.out.println(field.getInt(object));
 //		test(loader2);
 //		test(loader3);
-	}
+    }
 
-	public static void test(ClassLoader loader) {
-		
-		try {
-			Class clazz = loader.loadClass("Sample1");
-			Object object = clazz.newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void test(ClassLoader loader) {
+
+        try {
+            Class clazz = loader.loadClass("Sample1");
+            Object object = clazz.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
