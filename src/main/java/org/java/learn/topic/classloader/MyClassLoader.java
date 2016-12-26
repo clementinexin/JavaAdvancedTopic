25b7a805a93b65bbbe0da78aed185ae18f638129
daijiajia <daijiajia@meituan.com>
2016-12-26 13:36:22 +0800

    Files.readAllBytes(Paths.get(path + name.replace(".", "\\") + fileType))

--- src/main/java/org/java/learn/topic/classloader/MyClassLoader.java
+++ src/main/java/org/java/learn/topic/classloader/MyClassLoader.java
@@ -1,74 +1,74 @@
 package org.java.learn.topic.classloader;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import org.apache.commons.io.FileUtils;
 
 public class MyClassLoader extends ClassLoader {
 
-	private String name;
-	
-	private String path;
-	
-	private String fileType = ".class";
-	
-	public MyClassLoader(String name) {
-		super();
-		this.name = name;
-	}
-	
-	public MyClassLoader(ClassLoader parent,String name) {
-		super(parent);
-		this.name = name;
-	}
-	
-	@Override
-	protected Class<?> findClass(String name) throws ClassNotFoundException {
-		byte[] data = this.loadClassData(name);
-		
-		return this.defineClass(name, data, 0, data.length);
-	}
+    private String name;
 
-	private byte[] loadClassData(String name) {
-		try {
-			return FileUtils.readFileToByteArray(new File(path + name.replace(".", "\\") + fileType));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    private String path;
 
-	@Override
-	public String toString() {
-		return "MyClassLoader [name=" + name + "]";
-	}
+    private String fileType = ".class";
 
-	public static void main(String[] args) {
-		MyClassLoader loader1 = new MyClassLoader("loader1");
-		loader1.path = "D:\\exercise\\lib1\\";
-		
-		MyClassLoader loader2 = new MyClassLoader(loader1,"loader2");
-		loader2.path = "D:\\exercise\\lib2\\";
-		
-		MyClassLoader loader3 = new MyClassLoader(null, "loader3");
-		loader3.path = "D:\\exercise\\otherlib\\";
-		
-		test(loader1);
-		test(loader3);
-	}
+    public MyClassLoader(String name) {
+        super();
+        this.name = name;
+    }
 
-	public static void test(ClassLoader loader) {
-		
-		try {
-			Class clazz = loader.loadClass("Sample1");
-			Object object = clazz.newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    public MyClassLoader(ClassLoader parent, String name) {
+        super(parent);
+        this.name = name;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] data = this.loadClassData(name);
+
+        return this.defineClass(name, data, 0, data.length);
+    }
+
+    private byte[] loadClassData(String name) {
+        try {
+            return Files.readAllBytes(Paths.get(path + name.replace(".", "\\") + fileType));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClassLoader [name=" + name + "]";
+    }
+
+    public static void main(String[] args) {
+        MyClassLoader loader1 = new MyClassLoader("loader1");
+        loader1.path = "D:\\exercise\\lib1\\";
+
+        MyClassLoader loader2 = new MyClassLoader(loader1, "loader2");
+        loader2.path = "D:\\exercise\\lib2\\";
+
+        MyClassLoader loader3 = new MyClassLoader(null, "loader3");
+        loader3.path = "D:\\exercise\\otherlib\\";
+
+        test(loader1);
+        test(loader3);
+    }
+
+    public static void test(ClassLoader loader) {
+
+        try {
+            Class clazz = loader.loadClass("Sample1");
+            Object object = clazz.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
